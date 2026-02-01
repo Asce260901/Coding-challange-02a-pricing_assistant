036f8779b84fe2ce779b3690e3d8bf61686cfeec
Asce260901 <ajm55@usf.edu>
2026-02-01 17:50:18 -0500

    Name changes, phrase changes and correction of missing let

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -1,9 +1,9 @@
-let productName = "Sand"
+let productName = "Sanding pad"
 let costPerUnit = 26.35;
 let basePrice = 59.12;
 let discountRate = 0.05;
 let salesTaxRate = 0.075;
-fixedMonthlyCost = 1500;
+let fixedMonthlyCost = 1500;
 
 let discountedPrice = basePrice * (1 - discountRate);
 let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
@@ -11,9 +11,9 @@ let profitPerUnit = finalPriceWithTax - costPerUnit;
 let breakEvenUnits = Math.ceil(fixedMonthlyCost / profitPerUnit);
 let isProfitablePerUnit = profitPerUnit > 0 ;
 
-console.log( "Product:" +  productName,)
-console.log(`Price after discount:$${discountedPrice}`,) 
-console.log(`Final Price:$${finalPriceWithTax}`,)
-console.log(`Profit:$${profitPerUnit}`,)
-console.log("Break Even Units:" + breakEvenUnits,)
-console.log("Is Profitable?:" + isProfitablePerUnit,)
+console.log( "The product is:" +  productName,)
+console.log(`The price after discount:$${discountedPrice}`,) 
+console.log(`The client pays in total:$${finalPriceWithTax}`,)
+console.log(`Your profit:$${profitPerUnit}`,)
+console.log("The breakeven units are:" + breakEvenUnits,)
+console.log("Is it profitable to the company?:" + isProfitablePerUnit,)
