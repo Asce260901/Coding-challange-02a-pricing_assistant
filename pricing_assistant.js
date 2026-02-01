292dfc2c0e584b625d1687acded82eefc1214ec0
Asce260901 <ajm55@usf.edu>
2026-02-01 18:22:58 -0500

    added missing semicollons

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -1,4 +1,4 @@
-let productName = "Sanding pad"
+let productName = "Sanding pad";
 let costPerUnit = 26.35;
 let basePrice = 59.12;
 let discountRate = 0.05;
@@ -11,9 +11,9 @@ let profitPerUnit = finalPriceWithTax - costPerUnit;
 let breakEvenUnits = Math.ceil(fixedMonthlyCost / profitPerUnit);
 let isProfitablePerUnit = profitPerUnit > 0 ;
 
-console.log( "The product is:" +  productName,)
-console.log(`The price after discount:$${discountedPrice}`,) 
-console.log(`The client pays in total:$${finalPriceWithTax}`,)
-console.log(`Your profit:$${profitPerUnit}`,)
-console.log("The breakeven units are:" + breakEvenUnits,)
-console.log("Is it profitable to the company?:" + isProfitablePerUnit,)
+console.log( "The product is:" +  productName,);
+console.log(`The price after discount:$${discountedPrice}`,);
+console.log(`The client pays in total:$${finalPriceWithTax}`,);
+console.log(`Your profit:$${profitPerUnit}`,);
+console.log("The breakeven units are:" + breakEvenUnits,);
+console.log("Is it profitable to the company?:" + isProfitablePerUnit,);
